5b5d0c335225e005bac4ff15b1f13d3f7ddbb792
jayaprasad356 <jayaprasad356@gmail.com>
2022-09-30 15:53:40 +0530

    api updated

--- api/company_list.php
+++ api/company_list.php
@@ -11,24 +11,32 @@ include_once('../includes/crud.php');
 
 $db = new Database();
 $db->connect();
-$offset = $db->escapeString($_POST['offset']);
-$limit = $db->escapeString($_POST['limit']);
-$sql = "SELECT * FROM bank_cmp_cat,banks WHERE bank_cmp_cat.bank_name=banks.id LIMIT $offset,$limit";
-$db->sql($sql);
-$res = $db->getResult();
-$num = $db->numRows($res);
-if ($num >= 1) {
-    $response['success'] = true;
-    $response['message'] = "Company listed Successfully";
-    $response['total'] = $num;
-    $response['data'] = $res;
-    print_r(json_encode($response));
 
-}else{
-    $response['success'] = false;
-    $response['message'] = "No Company Found";
-    print_r(json_encode($response));
+
+if(isset($_POST['offset']) && isset($_POST['limit']) ){
+    $offset = $db->escapeString($_POST['offset']);
+    $limit = $db->escapeString($_POST['limit']);
+    $sql = "SELECT SQL_NO_CACHE bank_cmp_cat.id,bank_cmp_cat.company_name,bank_cmp_cat.cat,banks.bank_name FROM bank_cmp_cat,banks WHERE bank_cmp_cat.bank_name=banks.id LIMIT $offset,$limit";
+    //$sql = "SELECT SQL_NO_CACHE id,company_name,cat,bank_name FROM bank_cmp_cat ORDER BY id DESC LIMIT $offset,$limit";
+    
+    $db->sql($sql);
+    $res = $db->getResult();
+    $num = $db->numRows($res);
+    if ($num >= 1) {
+        $response['success'] = true;
+        $response['message'] = "Company listed Successfully";
+        $response['total'] = $num;
+        $response['data'] = $res;
+        print_r(json_encode($response));
+
+    }else{
+        $response['success'] = false;
+        $response['message'] = "No Company Found";
+        print_r(json_encode($response));
+
+    }
 
 }
+
 
 ?>
