b817df85e934fc8cb46ece7ac64dc9849fef10bb
jayaprasad356 <jayaprasad356@gmail.com>
2022-10-14 22:46:18 +0530

    bank list fixed

--- api/bank_list.php
+++ api/bank_list.php
@@ -26,7 +26,14 @@ if (empty($_POST['bank_id'])) {
 }
 $bank_id = $db->escapeString($_POST['bank_id']);
 $company_name = $db->escapeString($_POST['company_name']);
-$sql = "SELECT bank_cmp_cat.id,bank_cmp_cat.cat,bank_cmp_cat.remarks,banks.bank_name FROM bank_cmp_cat,banks WHERE bank_cmp_cat.bank_name=banks.id AND bank_cmp_cat.company_name = '$company_name' AND banks.id = '$bank_id' LIMIT 25";
+if($bank_id == 'all'){
+    $sql = "SELECT bank_cmp_cat.id,bank_cmp_cat.cat,bank_cmp_cat.remarks,banks.bank_name FROM bank_cmp_cat,banks WHERE bank_cmp_cat.bank_name=banks.id AND bank_cmp_cat.company_name = '$company_name' LIMIT 25";
+
+}
+else{
+    $sql = "SELECT bank_cmp_cat.id,bank_cmp_cat.cat,bank_cmp_cat.remarks,banks.bank_name FROM bank_cmp_cat,banks WHERE bank_cmp_cat.bank_name=banks.id AND bank_cmp_cat.company_name = '$company_name' AND banks.id = '$bank_id' LIMIT 25";
+
+}
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
